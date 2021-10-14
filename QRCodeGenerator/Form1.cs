d2fe211c4600ff4f6b65834f4fc08e9352bd5eb9
error404-69-dotcom <robocookieofficial@gmail.com>
2021-10-14 13:46:36 +0200

    Fixed several bugs
    Now every invalid filename character is deleted on save filename automatically

--- QRCodeGenerator/Form1.cs
+++ QRCodeGenerator/Form1.cs
@@ -13,12 +13,14 @@ using System.Web.UI.WebControls;
 using System.Net;
 using System.IO;
 using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
 using Image = System.Drawing.Image;
 
 namespace QRCodeGenerator
 {
     public partial class frmQRCodeGen : Form
     {
+        public static string name;
         public frmQRCodeGen()
         {
             InitializeComponent();
@@ -50,20 +52,33 @@ namespace QRCodeGenerator
         {
             if (txtLink.Text != "")
             {
-                string name = txtLink.Text.Replace("/", "");
-                var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", txtLink.Text, 180, 180);
-                WebResponse response = default(WebResponse);
-                Stream remoteStream = default(Stream);
-                StreamReader readStream = default(StreamReader);
-                WebRequest request = WebRequest.Create(url);
-                response = request.GetResponse();
-                remoteStream = response.GetResponseStream();
-                readStream = new StreamReader(remoteStream);
-                System.Drawing.Image img = System.Drawing.Image.FromStream(remoteStream);
-                img.Save($"{name}.png");
-                response.Close();
-                remoteStream.Close();
-                readStream.Close();
+                try
+                {
+                    var regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+                    var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+                    name = r.Replace(txtLink.Text, "");
+                    if (name.Length > 20)
+                        name = name.Substring(0, 20);
+                    var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", txtLink.Text, 180, 180);
+                    WebResponse response = default(WebResponse);
+                    Stream remoteStream = default(Stream);
+                    StreamReader readStream = default(StreamReader);
+                    WebRequest request = WebRequest.Create(url);
+                    response = request.GetResponse();
+                    remoteStream = response.GetResponseStream();
+                    readStream = new StreamReader(remoteStream);
+                    System.Drawing.Image img = System.Drawing.Image.FromStream(remoteStream);
+                    Console.WriteLine(name);
+                    Console.WriteLine(name.Length);
+                    img.Save($@".\{name}.png");
+                    response.Close();
+                    remoteStream.Close();
+                    readStream.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Could not save the QR code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
             else
             {
@@ -107,7 +122,7 @@ namespace QRCodeGenerator
                     }
                     catch (Exception)
                     {
-                        MessageBox.Show("There was an error while trying to copy the QR code", "Error", MessageBoxButtons.Ok, MessageBoxIcon.Error);
+                        MessageBox.Show("There was an error while trying to copy the QR code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
                 else if (question == DialogResult.No)
