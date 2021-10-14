cf8f9957b4e61f197e501b945d25bdc795d8e6a6
error404-69-dotcom <robocookieofficial@gmail.com>
2021-10-14 09:12:01 +0200

    init

---
+++ QRCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace QRCodeGenerator
+{
+    partial class frmQRCodeGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQRCodeGen));
+            this.btnGen = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.lblSuccess = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picbxCode = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(13, 52);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(105, 23);
+            this.btnGen.TabIndex = 0;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(13, 26);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(210, 20);
+            this.txtLink.TabIndex = 1;
+            // 
+            // lblSuccess
+            // 
+            this.lblSuccess.AutoSize = true;
+            this.lblSuccess.Location = new System.Drawing.Point(12, 78);
+            this.lblSuccess.Name = "lblSuccess";
+            this.lblSuccess.Size = new System.Drawing.Size(0, 13);
+            this.lblSuccess.TabIndex = 2;
+            this.lblSuccess.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Link";
+            // 
+            // picbxCode
+            // 
+            this.picbxCode.Location = new System.Drawing.Point(13, 81);
+            this.picbxCode.Name = "picbxCode";
+            this.picbxCode.Padding = new System.Windows.Forms.Padding(2);
+            this.picbxCode.Size = new System.Drawing.Size(209, 200);
+            this.picbxCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbxCode.TabIndex = 9;
+            this.picbxCode.TabStop = false;
+            this.picbxCode.Click += new System.EventHandler(this.picbxCode_MouseDoubleClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(13, 289);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(209, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(118, 52);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(29, 320);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblInfo.TabIndex = 12;
+            // 
+            // frmQRCodeGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 342);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.picbxCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSuccess);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.btnGen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmQRCodeGen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRGen";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label lblSuccess;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox picbxCode;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ QRCodeGenerator/QRCodeGenerator/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Drawing.Imaging;
+using Image = System.Drawing.Image;
+
+namespace QRCodeGenerator
+{
+    public partial class frmQRCodeGen : Form
+    {
+        public frmQRCodeGen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtLink.Text != "")
+                {
+                    var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", txtLink.Text, 180, 180);
+                    picbxCode.ImageLocation = url;
+
+                    lblInfo.Text = "Click the QR code to copy it";
+                }
+                else
+                {
+                    MessageBox.Show("Please give a link", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not create the QR code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtLink.Text != "")
+            {
+                string name = txtLink.Text.Replace("/", "");
+                var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", txtLink.Text, 180, 180);
+                WebResponse response = default(WebResponse);
+                Stream remoteStream = default(Stream);
+                StreamReader readStream = default(StreamReader);
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+                remoteStream = response.GetResponseStream();
+                readStream = new StreamReader(remoteStream);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(remoteStream);
+                img.Save($"{name}.png");
+                response.Close();
+                remoteStream.Close();
+                readStream.Close();
+            }
+            else
+            {
+                MessageBox.Show("Could not save the QR code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtLink.Text = string.Empty;
+                lblInfo.Text = string.Empty;
+                picbxCode.Image = null;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not clear the application", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void picbxCode_MouseDoubleClick(object sender, EventArgs e)
+        {
+            DialogResult question = MessageBox.Show("Do you want to copy the QR code to your clipboard?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (question == DialogResult.Yes)
+            {
+                Image img = new Bitmap(picbxCode.Width, picbxCode.Height);
+
+                Graphics g = Graphics.FromImage(img);
+
+                g.CopyFromScreen(PointToScreen(picbxCode.Location), new Point(0, 0), new Size(picbxCode.Width, picbxCode.Height));
+
+                Clipboard.SetImage(img);
+
+                g.Dispose();
+            }
+            else if (question == DialogResult.No)
+            {
+                return;
+            }
+        }
+    }
+}
